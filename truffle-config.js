3550401aa62a36ccbac30872588fa6130f86ab82
Solange Gueiros <solangegueiros@gmail.com>
2021-03-29 21:39:20 -0300

    update 2021-03

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,33 @@ const mnemonic = fs.readFileSync(".secret").toString().trim();
   console.log('unable to retrieve mnemonic from .secret');
 }
 
+
+//Update gas price Testnet
+/* Run this first, to use the result in truffle-config:
+  curl https://public-node.testnet.rsk.co/ -X POST -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false],"id":1}' \
+    > .minimum-gas-price-testnet.json
+*/
+const gasPriceTestnetRaw = fs.readFileSync(".minimum-gas-price-testnet.json").toString().trim();
+const minimumGasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result.minimumGasPrice, 16);
+if (typeof minimumGasPriceTestnet !== 'number' || isNaN(minimumGasPriceTestnet)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+}
+console.log("Minimum gas price Testnet: " + minimumGasPriceTestnet);
+
+//Update gas price Mainnet
+/* Run this first, to use the result in truffle-config:
+  curl https://public-node.rsk.co/ -X POST -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false],"id":1}' \
+    > .minimum-gas-price-mainnet.json
+*/
+const gasPriceMainnetRaw = fs.readFileSync(".minimum-gas-price-mainnet.json").toString().trim();
+const minimumGasPriceMainnet = parseInt(JSON.parse(gasPriceMainnetRaw).result.minimumGasPrice, 16);
+if (typeof minimumGasPriceMainnet !== 'number' || isNaN(minimumGasPriceMainnet)) {
+  throw new Error('unable to retrieve network gas price from .gas-price-mainnet.json');
+}
+console.log("Minimum gas price Mainnet: " + minimumGasPriceMainnet);
+
 module.exports = {
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
 
@@ -19,10 +46,51 @@ module.exports = {
       port: 8545,
       network_id: "*"
     },
+    regtest: {
+      host: "127.0.0.1",
+      port: 4444,
+      network_id: "*"
+    },     
     goerli: {
       provider: () => new HDWalletProvider(mnemonic, 'https://rpc.goerli.mudit.blog/', 0, 10),
       network_id: 5
+    },    
+    mumbai: {
+      provider: () => new HDWalletProvider(mnemonic, "https://rpc-mumbai.matic.today"),
+      network_id: '80001'
+    },    
+    testnet: { //Testnet RSK with dPathEthereum = metamask addresses
+      //https://www.npmjs.com/package/@truffle/hdwallet-provider
+      //provider: () => new HDWalletProvider(mnemonic, 'https://public-node.testnet.rsk.co', 0, 10),
+      provider: () => new HDWalletProvider({
+        mnemonic: { phrase: mnemonic },
+        providerOrUrl: 'https://public-node.testnet.rsk.co',
+        numberOfAddresses: 10,
+        pollingInterval: 15e3 
+      }),
+      network_id: 31,
+      gasPrice: Math.floor(minimumGasPriceTestnet * 1.3),
+      networkCheckTimeout: 1e6, //1h = 36e5
+      //Source: https://dappsdev.org/blog/2021-02-24-how-to-configure-truffle-to-connect-to-rsk/
+      // Higher polling interval to check for blocks less frequently
+      // during deployment
+      deploymentPollingInterval: 15e3,  //15s = 15e3, default is 4e3
+      timeoutBlocks: 100,
     },
+    testnetRSK: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://public-node.testnet.rsk.co', 0, 10, true, "m/44'/37310'/0'/0/"),
+      network_id: 31,
+      gasPrice: Math.floor(minimumGasPriceTestnet * 1.3),
+      networkCheckTimeout: 1e6,
+      timeoutBlocks: 100,
+      deploymentPollingInterval: 15e3,      
+    },    
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://public-node.rsk.co', 0, 1, true, "m/44'/137'/0'/0/"),
+      network_id: 30,
+      gasPrice: Math.floor(minimumGasPriceMainnet * 1.02),
+      networkCheckTimeout: 1e9
+    } 
   },
 
   // Set default mocha options here, use special reporters etc.
